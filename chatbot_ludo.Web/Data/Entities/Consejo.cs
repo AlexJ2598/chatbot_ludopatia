3e4c388030ed0766eddc245335b9ace4955a538f
AlexJ2598 <alexis.hernandez074@gmail.com>
2024-09-12 00:00:27 -0600

    Migracione modificando la base de datos.
    Se explica el como se modifico (ver los comentarios) para hacer migraciones directamente desde el Entitie del controlador "Consejos"

--- chatbot_ludo.Web/Data/Entities/Consejo.cs
+++ chatbot_ludo.Web/Data/Entities/Consejo.cs
@@ -5,10 +5,16 @@
     public class Consejo
     {
         public int ID_Consejo {  get; set; } //Lave primaria
-        public string? Texto_Consejo { get; set;}
+        [MaxLength(250, ErrorMessage = "The field {0} only can contain {1} characters length.")] //Anotaciones para limitar el consejo a 150 caracteres max
+        [Required] //Requerido. No puede haber consejos vacios.
+        public string Texto_Consejo { get; set;}
+        [MaxLength(50)]
         public string Categoria {  get; set; } //prevesion, sintomas, apoyo, etc.
         public int Grado_Recomendacion { get; set; } //1 bajo, 2 medium, 3 hight
         public DateTime Fecha_Creacios {  get; set; }
 
+        //Para hacer los cambios en DB vamos a la consola, dir direccion del producto web 
+        //Corremos acorde a lo que modificamos: dotnet ef migrations add ModifyProducts dotnet ef database update -- Porque modificamos consejos.
+        //Solo corremos cuando, por ejemplo, modificamos longitud (son migraciones)
     }
 }
---
+++ chatbot_ludo.Web/Migrations/20240912054923_ModifyConsejos.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chatbot_ludo.Web.Migrations
+{
+    public partial class ModifyConsejos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Texto_Consejo",
+                table: "Consejos",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Categoria",
+                table: "Consejos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Texto_Consejo",
+                table: "Consejos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Categoria",
+                table: "Consejos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
