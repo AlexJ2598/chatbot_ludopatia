8e3cb7f621d53c3643f1a17174259a4cc2012ed2
AlexJ2598 <alexis.hernandez074@gmail.com>
2024-10-01 17:02:51 -0600

    LoginViewModel
    Se construye parte de la lógica de la LoginViewModel. Se importa un paquete nuget para el RelayCommand dentro del ICommand.
    
    Fuera de eso, logica basica más alertas de display en caso de que no ingrese datos o datos incorrectos de inicio de sesión.

--- chatbot_ludo.UIForms/chatbot_ludo.UIForms/ViewModels/LoginViewModel.cs
+++ chatbot_ludo.UIForms/chatbot_ludo.UIForms/ViewModels/LoginViewModel.cs
@@ -1,9 +1,50 @@
 ﻿namespace chatbot_ludo.UIForms.ViewModels
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command; //MvvmLightLibsStd10 importar desde los nuget
+    using Xamarin.Forms;
+
     public class LoginViewModel
     {
+        //Propiedades para la View de Login
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //Comando para el login Page
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login); //Instanciamos al metodo donde validamos la info de inicio de sesión.
+            }
+        }
+
+        public LoginViewModel()
+        {
+            this.Email = "alexis.hernandez074@gmail.com"; //Inicializamos las propiedades de lectura y escritura.
+            this.Password = "123456";
+        }
+
+        private async void Login() //Inicio de sesion.
+        {
+            if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Ingresa un email","Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Ingresa un Password", "Aceptar");
+                return;
+            }
+            //Condicones para corroborar correo.
+            if(!this.Email.Equals("alexis.hernandez074@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Usuario o contraseña incorrecta", "Aceptar");
+                return;
+            }
+            //Salio todo bien.
+            await Application.Current.MainPage.DisplayAlert("Ok", "Todo bien", "Aceptar");
+        }
     }
 }
